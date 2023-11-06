5a40b499de7a11f2aa81ac880a3ec40394550a71
Marten <marten.nommkyla@gmail.com>
2023-11-06 15:02:51 +0200

    function

---
+++ function/function2.py
@@ -0,0 +1,23 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    """Return the sum and difference of given variables num_a and num_b."""
+    print("I´m inside the funstion")
+
+# my_name_is (name)
+def my_name_is (name):
+    print("my name is {name}")
+
+# sum_six(num)`
+def sum_six(num):
+    return num + 6
+
+# sum_numbers()
+def sum_numbers(a, b):
+    return a + b
+
+# usd_to_eur()
+def usd_to_eur(dollar):
+    return 80 * dollar / 100
